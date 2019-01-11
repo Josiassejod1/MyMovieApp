e25c8b4f86fe92685914fdbe16bb6a11d9232b74
dalvin <dalvin.sejour@nbcuni.com>
2019-01-11 10:09:41 -0500

    Initial Commit

---
+++ MyMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovieApp
+//
+//  Created by Dalvin Sejour on 1/11/19.
+//  Copyright © 2019 Dalvin Sejour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
